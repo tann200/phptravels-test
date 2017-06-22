f32a16c12ea05eb2d4c3acd5104d30e62ec82a75
tann200 <tann200@gmail.com>
2017-06-22 22:46:41 +0300

    Add files via upload

---
+++ pages/adminLogin.js
@@ -0,0 +1,23 @@
+'use strict';
+let I;
+
+module.exports = {
+
+  _init() {
+    I = actor();
+  },
+
+  // setting locators
+  fields: {
+    email: 'email',
+    password: 'password'
+  },
+  submitButton: 'Login',
+
+  // introducing methods
+  sendForm(email, password) {
+    I.fillField(this.fields.email, email);
+    I.fillField(this.fields.password, password);
+    I.click(this.submitButton);
+  }
+}
